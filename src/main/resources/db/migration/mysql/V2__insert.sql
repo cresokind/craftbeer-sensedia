2f5ebd409768ae2998055d6e67ef94a74e0707a5
cresokind <cresokind@brasilcard.com>
2020-01-02 04:20:37 -0300

    Tentando corrigir o build no travis

--- src/main/resources/db/migration/mysql/V2__insert.sql
+++
@@ -1,5 +0,0 @@
-/*INSERT INTO `cerveja` (`id`, `descricao`, `estilo`, `ibu`, `teor_alcolico`, `preco`) VALUES
-(1, 'Roleta Russa New England IPA 500ml', 'IPA', 64, 6, 22),
-(2, 'Roleta Russa India Pale Ale IPA 500ml', 'IPA', 58, 5.6, 19.9),
-(3, 'Roleta Russa Imperial IPA 500ml <3', 'IPA', 120, 7.6, 22.2),
-(4, 'Faxe Extra Strong lata 1 litro', 'Euro Strong Lager', 20, 10, 22.2);*/
