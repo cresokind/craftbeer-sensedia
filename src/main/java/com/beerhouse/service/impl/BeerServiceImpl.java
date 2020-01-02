d5e464a6ad7c3a451646159fc2a12e331325bcae
cresokind <cresokind@brasilcard.com>
2020-01-02 04:00:32 -0300

    Added Swagger and EhCache

--- src/main/java/com/beerhouse/service/impl/BeerServiceImpl.java
+++ src/main/java/com/beerhouse/service/impl/BeerServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -31,16 +33,19 @@ public class BeerServiceImpl implements BeerService{
 		return Optional.ofNullable(this.beerRepository.findByName(name));
 	}
 	
+	@CachePut("listarTodasCervejas")
 	public Beer cadastrar(Beer beer) {
 		log.info("Cadastrando/alterando uma cerveja {}", beer);
 		return this.beerRepository.save(beer);
 	}
 	
+	@CachePut("listarTodasCervejas")
 	public void remover(Long id) {
 		log.info("Apagamendo o cadastro da cerveja {}", id);
 		this.beerRepository.delete(id);
 	}
 
+	@Cacheable("listarTodasCervejas")
 	public Page<Beer> buscarTodasCervejas(PageRequest pageRequest) {
 		log.info("Buscando todas as cervejas cadastradas");
 		return this.beerRepository.findAll(pageRequest);
